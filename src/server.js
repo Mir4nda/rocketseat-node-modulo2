8a8970669bf60403b7e22a9ca069cfd4780d0f4b
Nuno Miranda <91080643+Mir4nda@users.noreply.github.com>
2022-12-04 06:09:39 +0000

    n o w

--- src/server.js
+++ src/server.js
@@ -8,19 +8,9 @@ const express_1 = __importDefault(require("express"));
 const categories_routes_1 = require("./routes/categories.routes");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: [
-        "https://games.gdevelop-app.com",
-        "https://game-previews.gdevelop.io",
-        "http://127.0.0.1:3333",
-        "http://localhost:3333",
-    ],
-    allowedHeaders: [
-        "https://games.gdevelop-app.com",
-        "https://game-previews.gdevelop.io",
-        "http://127.0.0.1:3333",
-        "http://localhost:3333",
-    ],
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    origin: "*",
+    allowedHeaders: "*",
+    methods: "*",
 }));
 app.use(express_1.default.json());
 app.use(categories_routes_1.categoriesRoutes);
--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "https://games.gdevelop-app.com",
-      "https://game-previews.gdevelop.io",
-      "http://127.0.0.1:3333",
-      "http://localhost:3333",
-    ],
-    allowedHeaders: [
-      "https://games.gdevelop-app.com",
-      "https://game-previews.gdevelop.io",
-      "http://127.0.0.1:3333",
-      "http://localhost:3333",
-    ],
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    origin: "*",
+    allowedHeaders: "*",
+    methods: "*",
   })
 );
 
